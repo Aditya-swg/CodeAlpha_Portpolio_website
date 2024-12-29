67afaf5f83a481bde71881f427cc98b7010482e8
Aditya-swg <adityakhandekar35@gmail.com>
2024-12-29 22:39:01 +0530

    Add files via upload

---
+++ portp.js
@@ -0,0 +1,6 @@
+
+document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevent actual submission
+    alert('Thank you for reaching out!');
+  });
+  
